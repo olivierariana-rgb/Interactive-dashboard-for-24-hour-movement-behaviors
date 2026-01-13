2690dd4c414e2ab2a6ced3668d1c79f7d2c1c005
olivierariana-rgb <olivier.ariana@gmail.com>
2026-01-13 17:57:25 -0500

    Update 3_Simplex.py

--- pages/pages/3_Simplex.py
+++ pages/pages/3_Simplex.py
@@ -1,8 +1,1 @@
-import streamlit as st
-from utils import load_data, apply_sidebar_filters
 
-df, meta = load_data()
-df_f, _ = apply_sidebar_filters(df)
-
-st.header("Ternary Simplex")
-st.info("Paste your simplex block here (we'll rebuild wide_all inside this page).")
