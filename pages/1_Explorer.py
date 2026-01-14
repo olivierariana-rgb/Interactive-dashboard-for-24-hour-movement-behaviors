bce92385b99409747d3ec6c43dac0e75ac557974
olivierariana-rgb <olivier.ariana@gmail.com>
2026-01-13 19:21:07 -0500

    Delete pages directory

--- pages/1_Explorer.py
+++
@@ -1,5 +0,0 @@
-# pages/page1.py
-import streamlit as st
-
-st.title("Page 1")
-st.write("This is page 1.")
--- pages/3_Simplex.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Test page")
-st.write("If you see this, pages are working.")
