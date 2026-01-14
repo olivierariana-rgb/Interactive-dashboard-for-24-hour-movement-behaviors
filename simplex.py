c35c416e5e686c615cb599c452c5e2367dcc53cc
olivierariana-rgb <olivier.ariana@gmail.com>
2026-01-13 20:16:18 -0500

    Update simplex.py

--- simplex.py
+++ simplex.py
@@ -1,3 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
 # --------------------------------------------------
 # SIMPLEX — Comparison mode (no Explorer filters)
 # --------------------------------------------------
